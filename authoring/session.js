d2fe99885e8467b4629e608cdcdfe4359c795d3c
Nicholas Cartwright <nc@belugalearning.com>
2012-07-21 15:14:50 +0100

    delete local var session in session.js - req.session is hardly too verbose and makes instant sense!

--- authoring/session.js
+++ authoring/session.js
@@ -11,11 +11,10 @@ module.exports = function session(kcm, noAuthenticatePathnames) {
   return function session(req, res, next) {
     connectSession(req, res, function() {
       var pathname = urlParser.parse(req.url).pathname
-        , session = req.session
       
       switch (pathname) {
         case '/logout':
-          if (session) session.destroy()
+          if (req.session) req.session.destroy()
           res.redirect('/login')
           break
         case '/login':
@@ -25,10 +24,10 @@ module.exports = function session(kcm, noAuthenticatePathnames) {
               var rows = JSON.parse(b).rows
 
               if (!rows.length) { // incorrect credentials
-                if (session) session.destroy()
+                if (req.session) req.session.destroy()
                 res.render('sessions/login', { redir:req.body.redir })
               } else { // correct credentials
-                session.user = lastLoggedInUser = rows[0].doc
+                req.session.user = lastLoggedInUser = rows[0].doc
 
                 // is anyone else logged into the kcm?
                 var now = new Date()
@@ -47,11 +46,11 @@ module.exports = function session(kcm, noAuthenticatePathnames) {
 
                 if (authenticatedSession) {
                   // there's someone else logged in. For current user to continue (s)he needs to force log out the other user logged in.
-                  session.pendingLoginConfirmation = true
+                  req.session.pendingLoginConfirmation = true
                   res.render('sessions/confirm-login', { redir:req.body.redir, loggedInUser:authenticatedSession.user.name })
                 } else {
                   // nobody else logged in, continue
-                  session.isAuthenticated = true
+                  req.session.isAuthenticated = true
                   res.redirect(req.body.redir || '/')
                 }
               }
@@ -62,7 +61,7 @@ module.exports = function session(kcm, noAuthenticatePathnames) {
           break
         case '/confirm-login':
           if (req.body.confirm == 'on') {
-            session.pendingLoginConfirmation = false
+            req.session.pendingLoginConfirmation = false
 
             // log off the other user if (s)he is still logged on
             var allSessions = req.sessionStore.sessions
@@ -73,7 +72,7 @@ module.exports = function session(kcm, noAuthenticatePathnames) {
                 if (s.isAuthenticated) req.sessionStore.destroy(id)
               }
             }
-            session.isAuthenticated = true
+            req.session.isAuthenticated = true
             res.redirect(req.body.redir || '/')
           } else {
             // login cancelled - redirect to login page (& session will be destroyed)
@@ -84,8 +83,8 @@ module.exports = function session(kcm, noAuthenticatePathnames) {
           break
         default:
           if (
-            !session.isAuthenticated && !~noAuthStrings.indexOf(pathname) && !_.find(noAuthRegexps, function(re) { return re.test(pathname) })
-            || session.pendingLoginConfirmation
+            !req.session.isAuthenticated && !~noAuthStrings.indexOf(pathname) && !_.find(noAuthRegexps, function(re) { return re.test(pathname) })
+            || req.session.pendingLoginConfirmation
           ) {
             if (req.isXMLHttpRequest) res.send('You have been logged out', 401)
             else res.redirect('/login?redir='+req.url)
