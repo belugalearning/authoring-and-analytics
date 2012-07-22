c0825dea001d896d450bb9dd4415ba7a7f3a0bd9
Nicholas Cartwright <nc@belugalearning.com>
2012-07-22 17:27:27 +0100

    remove redundant modules

--- authoring/model/kcm-emitter.js
+++
@@ -1,7 +0,0 @@
-var createEmitter = require('./couchdb-emitter').createEmitter
-
-exports.create = function(kcmURI, since) {
-  var dbEmitter = createEmitter(kcmURI, since)
-  // 'concept node' 'pipeline' 'problem'??? 'relation'
-  return dbEmitter
-}
