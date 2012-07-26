5acb908462bcc23e9109e3b69ad0e9e99a08952a
Nicholas Cartwright <nc@belugalearning.com>
2012-07-26 12:44:57 +0100

    update kcm model
    emit initialised event
    rename docStore() -> storeDorDoc()
    add getDoc()
    add getDocClone()

--- authoring/kcm/index.js
+++ authoring/kcm/index.js
@@ -13,6 +13,7 @@ function KCM(config) {
   var self = this
   events.EventEmitter.call(self)
 
+  self.initialised = false
   self.couchServerURI = config.couchServerURI.match(/^(.+[^\/])\/?$/)[1]
   self.databaseName = config.authoring.kcmDatabaseName.match(/^(.+[^\/])\/?$/)[1]
   self.dbURI = format('%s/%s', self.couchServerURI, self.databaseName)
@@ -32,7 +33,7 @@ function KCM(config) {
     }
 
     JSON.parse(b).rows.forEach(function(row) {
-      var o = self.docStore(row.doc)
+      var o = self.storeForDoc(row.doc)
       if (o) o[row.id] = row.doc
     })
 
@@ -64,17 +65,20 @@ function KCM(config) {
             }
           }
         } else {
-          store = self.docStore(change.doc)
+          store = self.storeForDoc(change.doc)
           if (store) store[change.id] = change.doc
         }
 
         if (store) self.emit('change', change)
       })
+
+      self.initialised = true
+      self.emit('initialised')
     })
   })
 }
 
-KCM.prototype.docStore = function(doc) {
+KCM.prototype.storeForDoc = function(doc) {
   return ({
     'concept node': this.docStores.nodes
     , 'pipeline': this.docStores.pipelines
@@ -82,4 +86,28 @@ KCM.prototype.docStore = function(doc) {
     , 'relation': this.docStores.relations
     , 'tool': this.docStores.tools
   })[doc.type]
+}
+
+KCM.prototype.getDoc = function(id, type) {
+  var store
+  if (type) {
+    store = ({
+      'concept node': this.docStores.nodes
+      , 'pipeline': this.docStores.pipelines
+      , 'problem': this.docStores.problems
+      , 'relation': this.docStores.relations
+      , 'tool': this.docStores.tools
+    })[type]
+  } else {
+    for (var i=0; i<docStores.length; i++) {
+      store = docStores[i]
+      if (store[id]) break
+    }
+  }
+  if (store) return store[id]
+}
+
+KCM.prototype.getDocClone = function(id, type) {
+  var doc = this.getDoc(id, type)
+  if (doc) return JSON.parse(JSON.stringify(doc))
 }
