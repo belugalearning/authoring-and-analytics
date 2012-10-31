83d201df4afd92085b37bfeca023ca25139320c7
Nicholas Cartwright <nc@belugalearning.com>
2012-10-31 17:16:58 +0000

    reformat & comment app web service for legibility

--- app_web_service/index.js
+++ app_web_service/index.js
@@ -1,34 +1,39 @@
-var express = require('express');
+var express = require('express')
+  , loggingServiceInit = require('./logging_service')
+  , usersServiceInit = require('./users_service')
 
 var server = express.createServer()
-  , usersService
-;
-
-module.exports = function(config) {
-    server.post('/app-logging/upload', require('./logging_service')(config))
-
-    usersService = require('./users_service')(config);
-    server.post('/app-users/sync-users', usersService.syncUsers);
-    server.post('/app-users/check-nick-available', usersService.checkNickAvailable);
-    server.post('/app-users/get-user-matching-nick-password', usersService.getUserMatchingNickAndPassword);
-
-    var port = parseInt(config.appWebService.port, 10) || 3000;
-    server.listen(port);
-    console.log("AppWebService Express server listening on port %d in %s mode", server.address().port, server.settings.env);
-
-    return server;
-};
 
 // server config
 server.configure(function() {
-    server.use(express.bodyParser());
-    server.use(express.cookieParser());
-    server.use(express.methodOverride());
-    server.use(server.router);
-});
+  server.use(express.bodyParser())
+  server.use(express.cookieParser())
+  server.use(express.methodOverride())
+  server.use(server.router)
+})
+
 server.configure('development', function(){
-    server.use(express.errorHandler({ dumpExceptions: true, showStack: true })); 
-});
+  server.use(express.errorHandler({ dumpExceptions: true, showStack: true }))
+})
+
 server.configure('production', function(){
-    server.use(express.errorHandler()); 
-});
+  server.use(express.errorHandler())
+})
+
+module.exports = function(config) {
+  // logging service
+  server.post('/app-logging/upload', loggingServiceInit(config))
+
+  // users service
+  var usersService = usersServiceInit(config)
+  server.post('/app-users/sync-users', usersService.syncUsers)
+  server.post('/app-users/check-nick-available', usersService.checkNickAvailable)
+  server.post('/app-users/get-user-matching-nick-password', usersService.getUserMatchingNickAndPassword)
+
+  // listen
+  var port = parseInt(config.appWebService.port, 10) || 3000
+  server.listen(port)
+  console.log("AppWebService Express server listening on port %d in %s mode", server.address().port, server.settings.env)
+
+  return server
+}
